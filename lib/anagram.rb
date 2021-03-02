64e9fe45a9f406490d4dad072b6bcf790c1d1d99
speraza545 <s.peraza545@gmail.com>
2021-03-02 13:16:12 -0500

    commit tree 8808be7dc4a4b258089b7ac763b7a90ce2a6e6e0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require "pry"
+
+class Anagram  
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+      
+      def match(array)
+        new_arr = array.map do |potential_anagram|
+            if @word.length == potential_anagram.length
+                count = 0
+                @word.split("").each do |letter|
+                 potential_anagram.include?(letter) ? count += 1 : nil
+                end
+                count == @word.length ? potential_anagram : nil
+            end
+        end
+        new_arr.compact
+      end
+end
